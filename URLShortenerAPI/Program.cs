c1ebf3573a39608ce457d743630270e23e540305
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-04 14:21:17 -0300

    removing unnecessary comments

--- URLShortenerAPI/Program.cs
+++ URLShortenerAPI/Program.cs
@@ -14,10 +14,8 @@ namespace URLShortenerAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -36,7 +34,7 @@ namespace URLShortenerAPI
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
